c3d22e662ee863e2b5b19f4a756c9a6ab4c4e63f
Seth <sethlindberg@users.noreply.github.com>
2021-06-29 15:05:19 -0700

    make monsters real and kick them
    monsters now can't be moved into or through. If you try it kicks them (just prints a message) and there's a dummy function to handle enemy turns

--- engine.py
+++ engine.py
@@ -19,6 +19,10 @@ class Engine:
         self.player = player
         self.update_fov()
 
+    def handle_enemy_turns(self) -> None:
+        for entity in self.game_map.entities - {self.player}:
+            print(f'The {entity.name} would take a turn here if it could.')
+
     def handle_events(self, events: Iterable[Any]) -> None:
         for event in events:
             action = self.event_handler.dispatch(event)
@@ -27,7 +31,7 @@ class Engine:
                 continue
 
             action.perform(self, self.player)
-
+            self.handle_enemy_turns()
             self.update_fov()
 
     def update_fov(self) -> None:

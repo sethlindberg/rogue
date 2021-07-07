2cc531766b60d1c3fbf895e1ac2b2161c0cb0126
Seth <sethlindberg@users.noreply.github.com>
2021-07-06 18:48:42 -0700

    part 6: add components, ai
    adds a base ai and a few other things.
    
    here, stuff starts to get a little complicated.

---
+++ components/ai.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+from __future__ import annotations
+
+from typing import List, Tuple
+
+import numpy as np  # type: ignore
+import tcod
+
+from actions import Action
+from components.base_component import BaseComponent
+
+
+class BaseAI(Action, BaseComponent):
+    def perform(self) -> None:
+        raise NotImplementedError()
+
+    def get_path_to(self, dest_x: int, dest_y: int) -> List[Tuple[int, int]]:
+        """
+        Compute and return a path to the target position.
+
+        If there is no valid path, return an empty list.
+        """
+        # Copy the walkable array
+        cost = np.array(self.entity.gamemap.tiles["walkable"], dtype=np.int8)
+
+        for entity in self.entity.gamemap.entities:
+            # Check if an entity blocks movement
+            if entity.blocks_movement and cost[entity.x, entity.y]:
+                # Add the cost of a blocked position.
+                # A lower number means more enemies will crowd behind
+                # one anotehr in hallways.
+                # A higher number means enemies will take longer paths
+                # in order to surround the player.
+                cost[entity.x, entity.y] += 10
+
+        # Create a graph from the cost array and pass to pathfinder
+        graph = tcod.path.SimpleGraph(cost=cost, cardinal=2, diagonal=3)
+        pathfinder = tcod.path.Pathfinder(graph)
+
+        pathfinder.add_root((self.entity.x, self.entity.y))  # Start pos
+
+        # Copy the path to the destination and remove starting point
+        path: List[List[int]] = pathfinder.path_to((dest_x, dest_y))[
+            1:].tolist()
+
+        # Convert from List[List[int]] to List[Tuple[int, int]]
+        return [(index[0], index[1]) for index in path]

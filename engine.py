421f476d413af989a60bc6e15ff25516f2f274fc
Seth <sethlindberg@users.noreply.github.com>
2021-07-14 12:36:57 -0700

    add colors, renderbar, bufix
    add colors, a render bar for hp and more
    
    also fixes wrong numpad & arrow key bindings

--- engine.py
+++ engine.py
@@ -8,6 +8,7 @@ from tcod.console import Console
 from tcod.map import compute_fov
 
 from input_handlers import MainGameEventHandler
+from render_functions import render_bar
 
 if TYPE_CHECKING:
     from entity import Actor
@@ -42,10 +43,11 @@ class Engine:
     def render(self, console: Console, context: Context) -> None:
         self.game_map.render(console)
 
-        console.print(
-            x=1,
-            y=47,
-            string=f"HP: {self.player.fighter.hp}/{self.player.fighter.max_hp}",
+        render_bar(
+            console=console,
+            current_value=self.player.fighter.hp,
+            maximum_value=self.player.fighter.max_hp,
+            total_width=20,
         )
 
         context.present(console)

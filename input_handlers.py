bc68adb9293c46795518b882d2acbbfea3c7d756
Seth <sethlindberg@users.noreply.github.com>
2021-07-21 13:54:24 -0700

    add simple character sheet viewer

--- input_handlers.py
+++ input_handlers.py
@@ -204,6 +204,63 @@ class AskUserEventHandler(EventHandler):
         return MainGameEventHandler(self.engine)
 
 
+class CharacterScreenEventHandler(AskUserEventHandler):
+    TITLE = "Character Information"
+
+    def on_render(self, console: Console) -> None:
+        super().on_render(console)
+
+        if self.engine.player.x <= 30:
+            x = 40
+        else:
+            x = 30
+
+        y = 0
+
+        width = len(self.TITLE) + 4
+
+        console.draw_frame(
+            x=x,
+            y=y,
+            width=width,
+            height=7,
+            title=self.TITLE,
+            clear=True,
+            fg=(255, 255, 255),
+            bg=(0, 0, 0),
+        )
+
+        console.print(
+            x=x + 1,
+            y=y + 1,
+            string=f"Level: {self.engine.player.level.current_level}",
+        )
+
+        console.print(
+            x=x + 1,
+            y=y + 2,
+            string=f"XP: {self.engine.player.level.current_xp}",
+        )
+
+        console.print(
+            x=x + 1,
+            y=y + 3,
+            string=f"XP to next level: {self.engine.player.level.experience_to_next_level}",
+        )
+
+        console.print(
+            x=x + 1,
+            y=y + 4,
+            string=f"Attack: {self.engine.player.fighter.power}",
+        )
+
+        console.print(
+            x=x + 1,
+            y=y + 5,
+            string=f"Defense: {self.engine.player.fighter.defense}",
+        )
+
+
 class LevelUpEventHandler(AskUserEventHandler):
     TITLE = "Level Up"
 
@@ -517,6 +574,8 @@ class MainGameEventHandler(EventHandler):
             return InventoryActivateHandler(self.engine)
         elif key == tcod.event.K_d:
             return InventoryDropHandler(self.engine)
+        elif key == tcod.event.K_c:
+            return CharacterScreenEventHandler(self.engine)
         elif key == tcod.event.K_SLASH:
             return LookHandler(self.engine)
 
